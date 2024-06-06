188c063839b369980f87e866a18644cfd084b6f7
ParinazMehrabi <parinaz.mehrabi6527@gmail.com>
2024-06-07 00:55:28 +0330

    g-options added

--- client/src/main/java/view/MessengerController.java
+++ client/src/main/java/view/MessengerController.java
@@ -28,7 +28,8 @@ public class MessengerController implements Initializable
     }
 
     public void groupF(MouseEvent event)
-    {group.setStyle("-fx-border-width: 0 0 2 0 ");
+    {
+        group.setStyle("-fx-border-width: 0 0 2 0 ");
     }
 
     public void groupE(MouseEvent event) {
