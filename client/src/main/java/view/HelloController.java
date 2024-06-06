6536540d940a5fb9a432d97b84236cf8588eaa14
MinaAbbspr <Mina4434abbas@gmail.com>
2024-06-06 16:45:10 +0330

    fxml controller added

--- client/src/main/java/view/HelloController.java
+++ client/src/main/java/view/HelloController.java
@@ -1,14 +1,110 @@
 package view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private AnchorPane blackPane;
+
+    @FXML
+    private Button btn_login_signup;
+
+    @FXML
+    private ImageView invisible;
+
+    @FXML
+    private ImageView invisible1;
+
+    @FXML
+    private PasswordField invlpass;
+
+    @FXML
+    private Label lbl_inner;
+
+    @FXML
+    private Label lbl_welcome;
+
+    @FXML
+    private TextField log1;
+
+    @FXML
+    private TextField login;
+
+    @FXML
+    private VBox loginV;
+
+    @FXML
+    private HBox pas1;
+
+    @FXML
+    private Label passInE1;
+
+    @FXML
+    private HBox signup;
+
+    @FXML
+    private VBox signupV;
+
+    @FXML
+    private PasswordField spas;
+
+    @FXML
+    private TextField svpass;
+
+    @FXML
+    private Label usernameES;
+
+    @FXML
+    private Label usernameInE1;
+
+    @FXML
+    private ImageView visible;
+
+    @FXML
+    private ImageView visible1;
+
+    @FXML
+    private TextField vlpass1;
+
+    @FXML
+    private HBox vpas1;
+
+    @FXML
+    private AnchorPane whitePane;
+
+    @FXML
+    void animation(MouseEvent event) {
+
     }
+
+    @FXML
+    void complete(MouseEvent event) {
+
+    }
+
+    @FXML
+    void invisible(MouseEvent event) {
+
+    }
+
+    @FXML
+    void login(MouseEvent event) {
+
+    }
+
+    @FXML
+    void visible(MouseEvent event) {
+
+    }
+
 }
